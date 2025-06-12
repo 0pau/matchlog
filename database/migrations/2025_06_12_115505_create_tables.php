307ae599a086600dce2a441d7d2c4f8c4711d78f
0pau <opauszkinorbi1@gmail.com>
2025-06-12 16:00:28 +0200

    Initial commit

---
+++ database/migrations/2025_06_12_115505_create_tables.php
@@ -0,0 +1,78 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasTable('competitions')) {
+            Schema::create('competitions', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->integer('year');
+                $table->string('theme');
+                $table->integer('p_correct');
+                $table->integer('p_incorrect');
+                $table->integer('p_empty');
+            });
+        }
+        if (!Schema::hasTable('rounds')) {
+            Schema::create('rounds', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->date('date');
+                $table->unsignedInteger('comp_id');
+
+                $table->foreign('comp_id')->references('id')->on('competitions')->cascadeOnDelete();
+
+            });
+        }
+        if (!Schema::hasTable('competitors')) {
+            Schema::create('competitors', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->date('birth');
+                $table->string('address');
+            });
+        }
+        if (!Schema::hasTable('part_of')) {
+            Schema::create('part_of', function (Blueprint $table) {
+                $table->unsignedInteger('round_id');
+                $table->unsignedInteger('competitor_id');
+
+                $table->foreign('round_id')->references('id')->on('rounds')->cascadeOnDelete();
+                $table->foreign('competitor_id')->references('id')->on('competitors')->cascadeOnDelete();
+            });
+        }
+        if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->string('email');
+                $table->string('password');
+                $table->string('phone_number');
+                $table->string('address');
+                $table->boolean('is_admin');
+
+                $table->primary('email');
+            });
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rounds');
+        Schema::dropIfExists('competitions');
+        Schema::dropIfExists('competitors');
+        Schema::dropIfExists('part_of');
+        Schema::dropIfExists('users');
+    }
+};
---
+++ resources/views/main.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+@include('partials.head')
+<body>
+    @include('partials.navbar');
+    <div class="container">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-md-10">
+                <h3 class="col-10">Versenyek</h3>
+                <p>Ez a lista jelenleg üres</p>
+            </div>
+            <div class="col"></div>
+        </div>
+    </div>
+</body>
+</html>
